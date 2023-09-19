5f1fd46d0f67de81cdcf8088a97c9c7f101801a1
w3suryadev <142774321+w3suryadev@users.noreply.github.com>
2023-09-19 22:34:59 +0530

    first commit

---
+++ ObjectDetection-CoreML/ViewController.swift
@@ -0,0 +1,249 @@
+//
+//  ViewController.swift
+//  SSDMobileNet-CoreML
+//
+//  Created by GwakDoyoung on 01/02/2019.
+//  Copyright © 2019 tucan9389. All rights reserved.
+//
+
+import UIKit
+import Vision
+import CoreMedia
+
+class ViewController: UIViewController {
+
+    // MARK: - UI Properties
+    @IBOutlet weak var videoPreview: UIView!
+    @IBOutlet weak var boxesView: DrawingBoundingBoxView!
+    @IBOutlet weak var labelsTableView: UITableView!
+    
+    @IBOutlet weak var inferenceLabel: UILabel!
+    @IBOutlet weak var etimeLabel: UILabel!
+    @IBOutlet weak var fpsLabel: UILabel!
+    
+    // MARK - Core ML model
+    // YOLOv3(iOS12+), YOLOv3FP16(iOS12+), YOLOv3Int8LUT(iOS12+)
+    // YOLOv3Tiny(iOS12+), YOLOv3TinyFP16(iOS12+), YOLOv3TinyInt8LUT(iOS12+)
+    // MobileNetV2_SSDLite(iOS12+), ObjectDetector(iOS12+)
+    // yolov5n(iOS13+), yolov5s(iOS13+), yolov5m(iOS13+), yolov5l(iOS13+), yolov5x(iOS13+)
+    // yolov5n6(iOS13+), yolov5s6(iOS13+), yolov5m6(iOS13+), yolov5l6(iOS13+), yolov5x6(iOS13+)
+    // yolov8n(iOS14+), yolov8s(iOS14+), yolov8m(iOS14+), yolov8l(iOS14+), yolov8x(iOS14+)
+//    lazy var objectDectectionModel = { return try? yolov8s() }()
+//    lazy var objectDectectionModel = { return try? dribble_nms() }()
+//    lazy var objectDectectionModel = { return try? dribble_n() }()
+//    lazy var objectDectectionModel = { return try? bb_n() }()
+    lazy var objectDectectionModel = { return try? dribble_latest() }()
+//    lazy var objectDectectionModel = { return try? bb_n_fp16() }()
+//    lazy var objectDectectionModel = { return try? dribble_224() }()
+    
+    // MARK: - Vision Properties
+    var request: VNCoreMLRequest?
+    var visionModel: VNCoreMLModel?
+    var isInferencing = false
+    
+    // MARK: - AV Property
+    var videoCapture: VideoCapture!
+    let semaphore = DispatchSemaphore(value: 1)
+    var lastExecution = Date()
+    
+    // MARK: - TableView Data
+    var predictions: [VNRecognizedObjectObservation] = []
+    
+    // MARK - Performance Measurement Property
+    private let PerformanceMeasurementProperty = PerformanceMeasurement()
+    
+    let maf1 = MovingAverageFilter()
+    let maf2 = MovingAverageFilter()
+    let maf3 = MovingAverageFilter()
+    
+    // MARK: - View Controller Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // setup the model
+        setUpModel()
+        
+        // setup camera
+        setUpCamera()
+        
+        // setup delegate for performance measurement
+        PerformanceMeasurementProperty.delegate = self
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.videoCapture.start()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.videoCapture.stop()
+    }
+    
+    // MARK: - Setup Core ML
+    func setUpModel() {
+        guard let objectDectectionModel = objectDectectionModel else { fatalError("fail to load the model") }
+
+        if let visionModel = try? VNCoreMLModel(for: objectDectectionModel.model) {
+            self.visionModel = visionModel
+            request = VNCoreMLRequest(model: visionModel, completionHandler: visionRequestDidComplete)
+            request?.imageCropAndScaleOption = .scaleFill
+           
+        } else {
+            fatalError("fail to create vision model")
+        }
+    }
+
+    // MARK: - SetUp Video
+    func setUpCamera() {
+        videoCapture = VideoCapture()
+        videoCapture.delegate = self
+        videoCapture.fps = 30
+        videoCapture.setUp(sessionPreset: .vga640x480) { success in
+            
+            if success {
+                // add preview view on the layer
+                if let previewLayer = self.videoCapture.previewLayer {
+                    self.videoPreview.layer.addSublayer(previewLayer)
+                    self.resizePreviewLayer()
+                }
+                
+                // start video preview when setup is done
+                self.videoCapture.start()
+            }
+        }
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        resizePreviewLayer()
+    }
+    
+    func resizePreviewLayer() {
+        videoCapture.previewLayer?.frame = videoPreview.bounds
+    }
+}
+
+// MARK: - VideoCaptureDelegate
+extension ViewController: VideoCaptureDelegate {
+    func videoCapture(_ capture: VideoCapture, didCaptureVideoFrame pixelBuffer: CVPixelBuffer?, timestamp: CMTime) {
+        // the captured image from camera is contained on pixelBuffer
+        if !self.isInferencing, let pixelBuffer = pixelBuffer {
+            self.isInferencing = true
+            
+            // start of measure
+            self.PerformanceMeasurementProperty.StartMeasurement()
+            
+            // predict!
+            self.predictUsingVision(pixelBuffer: pixelBuffer)
+        }
+    }
+}
+
+extension ViewController {
+    func predictUsingVision(pixelBuffer: CVPixelBuffer) {
+        guard let request = request else { fatalError() }
+        // vision framework configures the input size of image following our model's input configuration automatically
+        self.semaphore.wait()
+        let handler = VNImageRequestHandler(cvPixelBuffer: pixelBuffer)
+        try? handler.perform([request])
+    }
+    
+    // MARK: - Post-processing
+    func visionRequestDidComplete(request: VNRequest, error: Error?) {
+        self.PerformanceMeasurementProperty.labeling(with: "endInference")
+        if let predictions = request.results as? [VNRecognizedObjectObservation] {
+//            print(predictions.first?.labels.first?.identifier ?? "nil")
+//            print(predictions.first?.labels.first?.confidence ?? -1)
+            
+            self.predictions = predictions
+            print("------------------\n")
+            print(predictions)
+            print("------------------\n")
+            DispatchQueue.main.async {
+                self.boxesView.predictedObjects = predictions
+                self.labelsTableView.reloadData()
+
+                // end of measure
+                self.PerformanceMeasurementProperty.StopMeasurement()
+                
+                self.isInferencing = false
+            }
+        } else {
+            // end of measure
+            self.PerformanceMeasurementProperty.StopMeasurement()
+            
+            self.isInferencing = false
+        }
+        self.semaphore.signal()
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return predictions.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "InfoCell") else {
+            return UITableViewCell()
+        }
+
+        let confidenceSurya = predictions[indexPath.row].confidence
+        let predictionsSurya = predictions[indexPath.row]
+        
+        print("-------predictionsSurya-------\n\n")
+        print(predictionsSurya)
+        print("-------predictionsSurya-------\n\n")
+        
+        
+        let rectString = predictions[indexPath.row].boundingBox.toString(digit: 2)
+        let confidence = predictions[indexPath.row].labels.first?.confidence ?? -1
+        let confidenceString = String(format: "%.3f", confidence/*Math.sigmoid(confidence)*/)
+        
+        cell.textLabel?.text = predictions[indexPath.row].label ?? "N/A"
+//        cell.detailTextLabel?.text = "\(rectString), \(confidenceString)"
+        cell.detailTextLabel?.text = "\(confidenceSurya)"
+        return cell
+    }
+}
+
+// MARK: - 📏(Performance Measurement) Delegate
+extension ViewController: PerformanceMeasurementDelegate {
+    func updateMeasure(inferenceTime: Double, executionTime: Double, fps: Int) {
+        //print(executionTime, fps)
+        DispatchQueue.main.async {
+            self.maf1.append(element: Int(inferenceTime*1000.0))
+            self.maf2.append(element: Int(executionTime*1000.0))
+            self.maf3.append(element: fps)
+            
+            self.inferenceLabel.text = "inference: \(self.maf1.averageValue) ms"
+            self.etimeLabel.text = "execution: \(self.maf2.averageValue) ms"
+            self.fpsLabel.text = "fps: \(self.maf3.averageValue)"
+        }
+    }
+}
+
+class MovingAverageFilter {
+    private var arr: [Int] = []
+    private let maxCount = 10
+    
+    public func append(element: Int) {
+        arr.append(element)
+        if arr.count > maxCount {
+            arr.removeFirst()
+        }
+    }
+    
+    public var averageValue: Int {
+        guard !arr.isEmpty else { return 0 }
+        let sum = arr.reduce(0) { $0 + $1 }
+        return Int(Double(sum) / Double(arr.count))
+    }
+}
+
+
---
+++ ObjectDetection-CoreML/ultils/Measure.swift
@@ -0,0 +1,86 @@
+//
+//  Measure.swift
+//  TurtleApp-CoreML
+//
+//  Created by GwakDoyoung on 03/07/2018.
+//  Copyright © 2018 GwakDoyoung. All rights reserved.
+//
+
+import UIKit
+
+protocol PerformanceMeasurementDelegate {
+    func updateMeasure(inferenceTime: Double, executionTime: Double, fps: Int)
+}
+// Performance Measurement
+class PerformanceMeasurement {
+    
+    var delegate: PerformanceMeasurementDelegate?
+    
+    var index: Int = -1
+    var measurements: [Dictionary<String, Double>]
+    
+    init() {
+        let measurement = [
+            "start": CACurrentMediaTime(),
+            "end": CACurrentMediaTime()
+        ]
+        measurements = Array<Dictionary<String, Double>>(repeating: measurement, count: 30)
+    }
+    
+    // start
+    func StartMeasurement() {
+        index += 1
+        index %= 30
+        measurements[index] = [:]
+        
+        labeling(for: index, with: "start")
+    }
+    
+    // stop
+    func StopMeasurement() {
+        labeling(for: index, with: "end")
+        
+        let beforeMeasurement = getBeforeMeasurment(for: index)
+        let currentMeasurement = measurements[index]
+        if let startTime = currentMeasurement["start"],
+            let endInferenceTime = currentMeasurement["endInference"],
+            let endTime = currentMeasurement["end"],
+            let beforeStartTime = beforeMeasurement["start"] {
+            delegate?.updateMeasure(inferenceTime: endInferenceTime - startTime,
+                                    executionTime: endTime - startTime,
+                                    fps: Int(1/(startTime - beforeStartTime)))
+        }
+        
+    }
+    
+    // labeling with
+    func labeling(with msg: String? = "") {
+        labeling(for: index, with: msg)
+    }
+    
+    private func labeling(for index: Int, with msg: String? = "") {
+        if let message = msg {
+            measurements[index][message] = CACurrentMediaTime()
+        }
+    }
+    
+    private func getBeforeMeasurment(for index: Int) -> Dictionary<String, Double> {
+        return measurements[(index + 30 - 1) % 30]
+    }
+    
+    // log
+    func 🖨() {
+        
+    }
+}
+
+class MeasureLogView: UIView {
+    let etimeLabel = UILabel(frame: .zero)
+    let fpsLabel = UILabel(frame: .zero)
+    
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
